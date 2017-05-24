bad5752d3c0ef44046d336fd8a262481ba3c333e
Jonathan Pool <pool@stulta.com>
2017-05-24 16:27:57 -0700

    Created, tested, and debugged _without.

---
+++ spec/arrays/_without.js
@@ -0,0 +1,76 @@
+import chai, { expect } from 'chai'
+import _without from '../../src/arrays/_without'
+
+describe('_without', function() {
+
+  it('is a function', function() {
+    expect(_without).to.be.a('function')
+  })
+
+  context('valid arguments', function() {
+
+    it('_without() returns empty array when given an empty array', function() {
+      const emptyArray = []
+      expect(_without(emptyArray, 1)).to.deep.equal([])
+    })
+
+    it('_without() returns a pruned array with 2 arguments', function() {
+      let anArray = [1, 2, 3]
+      expect(_without(anArray, 2)).to.deep.equal([1, 3])
+    })
+    it('_without() returns a pruned array with 3 arguments', function() {
+      let anArray = [1, 2, 3]
+      expect(_without(anArray, 2, 1)).to.deep.equal([3])
+    })
+    it('_without() prunes multiple types', function() {
+      let anArray = [
+        '0', 1, 2, '3', false, true, undefined, Infinity
+      ]
+      expect(
+        _without(
+          anArray, '0', 7, 1, true, Infinity, undefined
+        )
+      ).to.deep.equal([2, '3', false])
+    })
+    it('_without() returns empty array if all values to be pruned', function() {
+      let anArray = [1, 2, 3]
+      expect(_without(anArray, 2, 1, 3)).to.deep.equal([])
+    })
+    it('_without() does not convert between number and string', function() {
+      let anArray = [1, '2', 3]
+      expect(_without(anArray, 2)).to.deep.equal([1, '2', 3])
+    })
+    it('_without() does not convert between number and boolean', function() {
+      let anArray = [1, 2, 0, 3]
+      expect(_without(anArray, false)).to.deep.equal([1, 2, 0, 3])
+    })
+
+  })
+
+  context('invalid arguments', function() {
+
+    it('_without() returns undefined when array is sparse', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_without(anArray, 'third')).to.be.undefined
+      expect(_without(anArray, 'fourth')).to.be.undefined
+    })
+    it('_without() returns undefined when argument 0 is a number', function() {
+      const nonArray = 2
+      expect(_without(nonArray, 2)).to.be.undefined
+      expect(_without(nonArray, 'anything')).to.be.undefined
+    })
+    it('_without() returns undefined when argument 0 is a string', function() {
+      const nonArray = 'stuff'
+      expect(_without(nonArray, 'stuff')).to.be.undefined
+    })
+    it(
+      '_without() returns undefined when argument 0 is a nonarray object',
+      function() {
+        const nonArray = {key: 1}
+        expect(_without(nonArray, 'key')).to.be.undefined
+    })
+
+  })
+
+})
---
+++ src/arrays/_without.js
@@ -0,0 +1,48 @@
+/*
+  Function declaration for _without and export statement making that
+  function the default export from this module.
+  Arguments:
+    array: array.
+    * (optional)
+  Preconditions:
+    0. array is a nonsparse array, none of whose elements in an array,
+      function, or object.
+*/
+export default function _without(array) {
+  // Define a function to validate an element.
+  const isValid = function(element) {
+    return (!(
+      Array.isArray(element) || ['function', 'object'].includes(typeof element)
+    ));
+  }
+  // If the arguments are valid:
+  if (
+    array !== undefined
+    && Array.isArray(array)
+    && array.length === Object.keys(array).length
+    && array.every(isValid)
+  ) {
+    // Initialize a result.
+    let result = [];
+    // Initialize an array of values to be pruned.
+    let prune = [];
+    // Identify an array of the values to be pruned.
+    for (let i = 1; i < arguments.length; i++) {
+      prune.push(arguments[i]);
+    }
+    // For each element of the array:
+    for (let i = 0; i < array.length; i++) {
+      // If it is not to be pruned:
+      if (! prune.includes(array[i])) {
+        // Append it to the result.
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
+  // Otherwise, i.e. if the arguments are invalid:
+  else {
+    // Return undefined.
+    return undefined;
+  }
+}
