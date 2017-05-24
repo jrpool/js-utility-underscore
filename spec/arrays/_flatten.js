dd0a4850162f57883a751da6be7dc9ebeab7cdf8
Jonathan Pool <pool@stulta.com>
2017-05-24 10:24:26 -0700

    Created, tested, and debugged _flatten.

---
+++ spec/arrays/_flatten.js
@@ -0,0 +1,146 @@
+import chai, { expect } from 'chai'
+import _flatten from '../../src/arrays/_flatten'
+
+describe('_flatten', function() {
+
+  it('is a function', function() {
+    expect(_flatten).to.be.a('function')
+  })
+
+  context('shallow === true', function() {
+
+    it('_flatten() returns empty array when given an empty array', function() {
+      const emptyArray = []
+      expect(_flatten(emptyArray, true)).to.deep.equal([])
+    })
+
+    it(
+      '_flatten() returns a copy of a 1-level array', function() {
+        let anArray = [1, 2, 3]
+        expect(_flatten(anArray, true)).to.deep.equal([1, 2, 3])
+      }
+    )
+    it(
+      '_flatten() converts a simple 2-level array to a 1-level array',
+      function() {
+        let anArray = [1, [2, 3], 4]
+        expect(_flatten(anArray, true)).to.deep.equal([1, 2, 3, 4])
+      }
+    )
+    it(
+      '_flatten() converts a complex 2-level array to a 1-level array',
+      function() {
+        let anArray = [
+          1, [2, 3], undefined, [4], [], {key: '5'}, null, 'string', NaN, ''
+        ]
+        expect(_flatten(anArray, true)).to.deep.equal([
+          1, 2, 3, undefined, 4, {key: '5'}, null, 'string', NaN, ''
+        ])
+      }
+    )
+    it('_flatten() converts a 3-level array to a 2-level array', function() {
+      let anArray = [1, [2, [3]], [4, [5, '6']], [[]]]
+      expect(_flatten(anArray, true))
+        .to.deep.equal([1, 2, [3], 4, [5, '6'], []])
+    })
+    it('_flatten() returns undefined when given a sparse array', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_flatten(anArray, true)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a number', function() {
+      const nonArray = 2
+      expect(_flatten(nonArray, true)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a string', function() {
+      const nonArray = 'stuff'
+      expect(_flatten(nonArray, true)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a nonarray object', function() {
+      const nonArray = {key: 1}
+      expect(_flatten(nonArray, true)).to.be.undefined
+    })
+
+  })
+
+  context('shallow === false', function() {
+
+    it('_flatten() returns empty array when given an empty array', function() {
+      const emptyArray = []
+      expect(_flatten(emptyArray, false)).to.deep.equal([])
+    })
+
+    it('_flatten() converts a 2-level array to a 1-level array', function() {
+      let anArray = [
+        1, [2, 3], undefined, [4], [], {key: '5'}, null, 'string', NaN, ''
+      ]
+      expect(_flatten(anArray, false)).to.deep.equal([
+        1, 2, 3, undefined, 4, {key: '5'}, null, 'string', NaN, ''
+      ])
+    })
+    it('_flatten() converts a 3-level array to a 1-level array', function() {
+      let anArray = [1, [2, [3]], [4, [5, '6']], [[]]]
+      expect(_flatten(anArray, false))
+        .to.deep.equal([1, 2, 3, 4, 5, '6'])
+    })
+    it('_flatten() returns undefined when given a sparse array', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_flatten(anArray, false)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a number', function() {
+      const nonArray = 2
+      expect(_flatten(nonArray, false)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a string', function() {
+      const nonArray = 'stuff'
+      expect(_flatten(nonArray, false)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a nonarray object', function() {
+      const nonArray = {key: 1}
+      expect(_flatten(nonArray, false)).to.be.undefined
+    })
+
+  })
+
+  context('shallow unspecified', function() {
+
+    it('_flatten() returns empty array when given an empty array', function() {
+      const emptyArray = []
+      expect(_flatten(emptyArray)).to.deep.equal([])
+    })
+
+    it('_flatten() converts a 2-level array to a 1-level array', function() {
+      let anArray = [
+        1, [2, 3], undefined, [4], [], {key: '5'}, null, 'string', NaN, ''
+      ]
+      expect(_flatten(anArray)).to.deep.equal([
+        1, 2, 3, undefined, 4, {key: '5'}, null, 'string', NaN, ''
+      ])
+    })
+    it('_flatten() converts a 3-level array to a 1-level array', function() {
+      let anArray = [1, [2, [3]], [4, [5, '6']], [[]]]
+      expect(_flatten(anArray))
+        .to.deep.equal([1, 2, 3, 4, 5, '6'])
+    })
+    it('_flatten() returns undefined when given a sparse array', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_flatten(anArray)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a number', function() {
+      const nonArray = 2
+      expect(_flatten(nonArray)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a string', function() {
+      const nonArray = 'stuff'
+      expect(_flatten(nonArray)).to.be.undefined
+    })
+    it('_flatten() returns undefined when given a nonarray object', function() {
+      const nonArray = {key: 1}
+      expect(_flatten(nonArray)).to.be.undefined
+    })
+
+  })
+
+})
---
+++ src/arrays/_flatten.js
@@ -0,0 +1,45 @@
+/*
+  Function declaration for _flatten and export statement making that
+  function the default export from this module.
+  Arguments:
+    array: array.
+    shallow (optional): whether to limit flattening to top level. Default false.
+  Preconditions:
+    0. array is a nonsparse array.
+    1. shallow is boolean.
+*/
+export default function _flatten(array, shallow = false) {
+  // If the arguments are valid:
+  if (
+    array !== undefined
+    && Array.isArray(array)
+    && array.length === Object.keys(array).length
+    && (shallow !== undefined || typeof shallow === 'boolean')
+  ) {
+    // Initialize a result.
+    let result = [];
+    // For each element:
+    for (let i = 0; i < array.length; i++) {
+      // If it is an array:
+      if (Array.isArray(array[i])) {
+        /*
+          Concatenate it (if shallow) or its flattened array (if not shallow)
+          to the result.
+        */
+        result = result.concat(shallow ? array[i] : _flatten(array[i]));
+      }
+      // Otherwise, i.e. if it is not an array:
+      else {
+        // Add it to the end of the result.
+        result.push(array[i]);
+      }
+    }
+    // Return the result.
+    return result;
+  }
+  // Otherwise, i.e. if the arguments are invalid:
+  else {
+    // Return undefined.
+    return undefined;
+  }
+}
