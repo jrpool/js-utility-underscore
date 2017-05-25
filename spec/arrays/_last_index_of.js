6f043092ed4e3601ff67db8fe5d1ca5d37ed4d75
Jonathan Pool <pool@stulta.com>
2017-05-25 10:58:57 -0700

    Arraylastindexof (#24)
    * Improved _union. Created, tested, debugged _intersection.
    
    * Improved _intersection. Created, tested, and debugged _difference.
    
    * Created, tested, and debugged _lastIndexOf.

---
+++ spec/arrays/_last_index_of.js
@@ -0,0 +1,116 @@
+import chai, { expect } from 'chai'
+import _lastIndexOf from '../../src/arrays/_last_index_of'
+
+describe('_lastIndexOf', function() {
+
+  it('is a function', function() {
+    expect(_lastIndexOf).to.be.a('function')
+  })
+
+  context('fromIndex not supplied', function() {
+    it('returns -1 when array is empty', function() {
+      expect(_lastIndexOf([], 'stuff')).to.equal(-1)
+    })
+    it('returns -1 when array does not include value', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf'], 'stuff')).to.equal(-1)
+    })
+    it('returns index when array includes 1 instance', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf'], 'b')).to.equal(1)
+    })
+    it('returns last index when array includes 2 instances', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf', 'b'], 'b')).to.equal(3)
+    })
+    it('returns last index when array includes 3 instances', function() {
+      expect(_lastIndexOf(['b', 7, 'b', 'stuf', 'b'], 'b')).to.equal(4)
+    })
+    it('matches numeric instances', function() {
+      expect(_lastIndexOf(['b', 7, 'b', 'stuf', 'b'], 7)).to.equal(1)
+    })
+    it('matches undefined instances', function() {
+      expect(_lastIndexOf([undefined, 7, 'b', 'stuf', undefined], undefined))
+        .to.equal(4)
+    })
+    it('matches Infinity instances', function() {
+      expect(_lastIndexOf(['b', 7, - Infinity, 'stuf'], - Infinity))
+        .to.equal(2)
+    })
+    it('matches NaN instances', function() {
+      expect(_lastIndexOf([NaN, 7, - Infinity, NaN, 'stuf'], - Infinity))
+        .to.equal(2)
+    })
+  })
+
+  context('fromIndex supplied', function() {
+    it('returns same result when fromIndex is 0', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf', 'b'], 'b', 0)).to.equal(3)
+    })
+    it('returns -1 when fromIndex is after last instance', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf', 33], 'stuff', 3)).to.equal(-1)
+    })
+    it('returns -1 when fromIndex is after array end', function() {
+      expect(_lastIndexOf(['a', 'b', 'stuf', 33], 'stuf', 8)).to.equal(-1)
+    })
+    it(
+      'returns same result when fromIndex is not after last instance',
+      function() {
+        expect(_lastIndexOf(['a', 'b', 'stuf', 33], 'stuf', 1)).to.equal(2)
+      }
+    )
+  })
+
+  context('sparse array', function() {
+    it('with no fromIndex returns undefined', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_lastIndexOf(anArray, 'third')).to.be.undefined
+    })
+    it('with fromIndex returns undefined', function() {
+      const anArray = []
+      anArray[2] = 'third'
+      expect(_lastIndexOf(anArray, 'third', 1)).to.be.undefined
+    })
+  })
+
+  context('invalid arguments', function() {
+    it('with argument 0 a number returns undefined', function() {
+      expect(_lastIndexOf(2)).to.be.undefined
+    })
+    it('with argument 0 a string returns undefined', function() {
+      expect(_lastIndexOf('[1, 2]')).to.be.undefined
+    })
+    it('with argument 0 a string and fromIndex returns undefined', function() {
+      expect(_lastIndexOf('[1, 2]', 0)).to.be.undefined
+    })
+    it('with argument 0 an object returns undefined', function() {
+      expect(_lastIndexOf({a: 1})).to.be.undefined
+    })
+    it('with argument 0 null returns undefined', function() {
+      expect(_lastIndexOf(null)).to.be.undefined
+    })
+    it('with argument 0 undefined returns undefined', function() {
+      expect(_lastIndexOf(undefined)).to.be.undefined
+    })
+    it('with 1 argument returns undefined', function() {
+      expect(_lastIndexOf([1, 2, 3])).to.be.undefined
+    })
+    it('with no argument returns undefined', function() {
+      expect(_lastIndexOf()).to.be.undefined
+    })
+    it('with argument 2 a string returns undefined', function() {
+      expect(_lastIndexOf([1, 2, 3], 2, '0')).to.be.undefined
+    })
+    it('with argument 2 a non-integer returns undefined', function() {
+      expect(_lastIndexOf([1, 2, 3], 2, 1.5)).to.be.undefined
+    })
+    it('with argument 2 negative returns undefined', function() {
+      expect(_lastIndexOf([1, 2, 3], 2, -1)).to.be.undefined
+    })
+    it('with argument 0 including an array returns undefined', function() {
+      expect(_lastIndexOf([1, 2, [3, 4], 5], 2)).to.be.undefined
+    })
+    it('with argument 0 including an object returns undefined', function() {
+      expect(_lastIndexOf([1, 2, {a: 3}, 5], 2, 1)).to.be.undefined
+    })
+  })
+
+})
---
+++ src/arrays/_last_index_of.js
@@ -0,0 +1,52 @@
+/*
+  Function declaration for _lastIndexOf and export statement making that
+  function the default export from this module.
+  Arguments:
+    array: array.
+    value: value to find last index of in array.
+    fromIndex (optional): index to start looking from.
+  Preconditions:
+    0. array is nonsparse.
+    1. array contains no object, array, or function.
+    2. fromIndex is a nonnegative integer.
+*/
+export default function _lastIndexOf(array, value, fromIndex = 0) {
+  // Define a function to validate an element.
+  const isValidEl = function(element) {
+    return (!(
+      Array.isArray(element) || ['function', 'object'].includes(typeof element)
+    ));
+  }
+  // If the arguments are superficially valid:
+  if (
+    arguments.length > 1
+    && arguments.length < 4
+    && array !== undefined
+    && Array.isArray(array)
+    && array.length === Object.keys(array).length
+    && (
+      fromIndex === undefined
+      || (
+        typeof fromIndex === 'number'
+        && Math.ceil(fromIndex) === Math.floor(fromIndex)
+        && fromIndex >= 0
+      )
+    )
+  ) {
+    // For each element of array:
+    for (let i = 0; i < array.length; i++) {
+      // If it is invalid:
+      if (! isValidEl(array[i])) {
+        return undefined;
+      }
+    }
+    // Return the result.
+    const lastIndex = array.slice(fromIndex).lastIndexOf(value);
+    return lastIndex > -1 ? lastIndex + fromIndex : -1;
+  }
+  // Otherwise, i.e. if the arguments are superficially invalid:
+  else {
+    // Return undefined.
+    return undefined;
+  }
+}
